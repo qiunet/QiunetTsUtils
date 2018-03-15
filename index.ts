10b00959f2eebf1ddd7be5b3b14a502a843871c7
qiunet <qiunet@163.com>
2018-03-15 11:16:31 +0800

    可以打包自己的ts工具包. 并且被引用了.

---
+++ index.ts
@@ -0,0 +1,6 @@
+export {BaseWebSocketClient} from './src/BaseWebSocketClient';
+export {ByteOutputBuffer} from './src/ByteOutputBuffer';
+export {ByteInputBuffer} from './src/ByteInputBuffer';
+export {BaseResponseHandler} from './src/ResponseHandler';
+export {ResponseMapping} from './src/ResponseHandler';
+export {ProtocolHeader} from './src/ProtocolHeader';
